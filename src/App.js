e0642cb77e69889245e00a6c34d76b8a71f8295b
Abdi Kumela <abdikumela036@gmail.com>
2024-03-23 10:17:10 +0300

    make a component for the game

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
- export default function App(){
-  return <div className="App">
-  <h1>Hello</h1>
-  </div>
- }
+import { useState } from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="board_row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board_row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board_row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </div>
+  );
+}
+
+function Square() {
+  const [value, setValue] = useState(null);
+  function handleClick() {
+    setValue("X");
+  }
+  return (
+    <div>
+      <button className="square" onClick={handleClick}>
+        {value}
+      </button>
+    </div>
+  );
+}
